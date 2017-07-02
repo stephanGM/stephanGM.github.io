ebdebc80e0470142d31b3acc062fe33e911c9e89
carbine <carbine@carbines-MBP.local>
2017-07-01 22:34:00 -0400

    mod small recap script

--- js/recap.js
+++ js/recap.js
@@ -2,7 +2,8 @@ var correctCaptcha = function(response) {
         if (response.length == 0){ // captcha unsolved
             alert("captcha failed")
         }else{ // captcha solved
+            alert(response);
             $(input).fadeIn('normal');
         }
-        alert(response);
+       
     };
