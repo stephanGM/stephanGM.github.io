8b587ad52844e38b766912cf6849895c5ba32a57
carbine <carbine@carbines-MBP.local>
2017-07-01 21:59:54 -0400

    mod small recap script

--- js/recap.js
+++ js/recap.js
@@ -1,3 +1,14 @@
-var correctCaptcha = function(response) {
-        alert(response);
-    };
+function get_action(form) 
+{
+    var v = grecaptcha.getResponse();
+    if(v.length == 0)
+    {
+        alert("false");
+        return false;
+    }
+    else
+    {
+        alert("false");
+        return true; 
+    }
+}
