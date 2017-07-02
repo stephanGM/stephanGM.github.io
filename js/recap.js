637cba1e210ae5fc5d0373e36d54e02d534ceaad
carbine <carbine@carbines-MBP.local>
2017-07-01 22:31:37 -0400

    mod small recap script

--- js/recap.js
+++ js/recap.js
@@ -1,3 +1,8 @@
 var correctCaptcha = function(response) {
+        if (response.length == 0){ // captcha unsolved
+            alert("captcha failed")
+        }else{ // captcha solved
+            $(input).fadeIn('normal');
+        }
         alert(response);
     };
