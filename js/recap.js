40fd3891eb6536355e60757fe0b92b0109f21eff
carbine <carbine@carbines-MBP.local>
2017-07-01 22:03:06 -0400

    mod small recap script

--- js/recap.js
+++ js/recap.js
@@ -1,14 +1,3 @@
-function get_action(form) 
-{
-    var v = grecaptcha.getResponse();
-    if(v.length == 0)
-    {
-        alert("false");
-        return false;
-    }
-    else
-    {
-        alert("false");
-        return true; 
-    }
-}
+var correctCaptcha = function(response) {
+        alert(response);
+    };
