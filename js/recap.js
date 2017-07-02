dd08a720691a1655d2da214a4112ad36d253177e
carbine <carbine@carbines-MBP.local>
2017-07-01 23:03:55 -0400

    mod small recap script

--- js/recap.js
+++ js/recap.js
@@ -2,7 +2,7 @@ var correctCaptcha = function(response) {
     if (response.length == 0){ // captcha unsolved
         alert("captcha failed")
     }else{ // captcha solved
-        $("input[type='submit']").hide(); 
-        $(".g-recaptcha").fadeOut('normal');
+        $(".g-recaptcha").hide('normal');
+        $("input[type='submit']").fadeIn(); 
     }      
 };
